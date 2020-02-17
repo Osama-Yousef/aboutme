74e94e3aa5dc86d6d1f3586ad3f37a46a695080b
Osama-Yousef <osamawalidyousef@gmail.com>
2020-02-17 19:48:34 +0200

    update

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict'
+
+var name = prompt("enter your name ?");
+console.log(name);
+console.log(typeof name);
+
+
+var locat = prompt("are you live in jordan ?");
+
+while (locat !== "yes" && locat !== "no") {
+  locat = prompt("are you live in jordan ? yes / no ");
+}
+ if (locat === "yes" ) { alert("WoW we are in the same country , nice to meet you ");}
+ else { alert("Oh noo ! you must visit us!");}
+
+
+
+         var userInput = prompt("are you feeling good?");
+
+
+         while (userInput !== "yes" && userInput !== "no") {
+        
+            userInput = prompt("are you feeling good ? yes / no ");
+          }
+
+          if (userInput === "yes" ) { alert(" i wish more good feelings to u ");}
+ else { alert(" try to do something you love , you will be better");}
+
+
+
+          
+
+            var educ = prompt("do you have education history ?");
+    
+
+            while (educ !== "yes" && educ !== "no") {
+                educ = prompt("do you have education history ? yes / no ");
+           
+             }
+
+             if (educ === "yes" ) { alert(" Niiice coz learning is a good thing ");}
+             else { alert(" you should work on your self more");}
+
+
+              var job = prompt("do you have job experience ?");
+        
+              while (job !== "yes" && job !== "no") {
+             job = prompt("do you have job experience ?  yes / no");
+                
+             }
+
+             if (job === "yes" ) { alert(" Amaziiing , the experience nowadays is useful to have a job ");}
+             else { alert(" So hooow you will get moneyyy!!!");}
+
+
+
+                 var purp = prompt("do you have purposes ?");
+
+                while (purp !== "yes" && purp !== "no") {
+                purp = prompt("do you have purposes ?  yes / no ");
+                   
+              }
+
+              if (purp === "yes" ) { alert(" You will be success,keep going to achieve them all ");}
+             else { alert(" So why youuu still a live hhh ");}
+
+
+               
+            
+
+            
