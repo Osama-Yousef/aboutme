fadec153a3d87997e28eefa732319ac076192460
Osama-Yousef <osamawalidyousef@gmail.com>
2020-02-18 23:31:13 +0200

    final update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aboutme
 
+* Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.Indicates through an alert if the guess is “too high” or “too low”.It should give the user exactly four opportunities to get the correct answer.After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+
+* Add a 7th question that has multiple possible correct answers that are stored in an array.Give the user 6 attempts to guess the correct answer.The guesses will end once the user guesses a correct answer or they run out of attempts.Display all the possible correct answers to the user.Consider using a loop of some sort for this question.
+* As a user, I would like to know my final score so that I can know how well I did.Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
--- js/app.js
+++ js/app.js
@@ -1,73 +1,134 @@
 'use strict';
+var score = 0;
 
-var name = prompt("enter your name ?");
-console.log(name);
-console.log(typeof name);
+ var name = prompt("enter your name ?");
+ console.log(name);
+ console.log(typeof name);
 
 
 
 
-var locat = prompt("are you live in jordan ?  (y/n) or (yes/no)").toLocaleLowerCase();
+ var locat = prompt("are you live in jordan ?  (y/n) or (yes/no)").toLocaleLowerCase();
 
-while (locat !== "yes" && locat !== "no" && locat !=="y" && locat !=="n") {
-  locat = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
+ while (locat !== "yes" && locat !== "no" && locat !== "y" && locat !== "n") {
+   locat = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
+ }
+
+
+ if (locat === "yes" || locat === "y") { alert("WoW we are in the same country , nice to meet you ");score++; }
+ else { alert("Oh noo ! you must visit us!"); }
+
+
+
+ var userInput = prompt("are you feeling good?  (y/n) or (yes/no) ").toLocaleLowerCase();
+
+
+ while (userInput !== "yes" && userInput !== "no" && userInput !== "y" && userInput !== "n") {
+
+   userInput = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
+ }
+
+ if (userInput === "yes" || userInput === "y") { alert(" i wish more good feelings to u ");score++; }
+ else { alert(" try to do something you love , you will be better"); }
+
+
+
+
+
+ var educ = prompt("do you have education history ?(y/n) or (yes/no)").toLocaleLowerCase();
+
+
+ while (educ !== "yes" && educ !== "no" && educ !== "y" && educ !== "n") {
+   educ = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
+
+ }
+
+ if (educ === "yes" || educ === "y") { alert(" Niiice coz learning is a good thing ");score++; }
+ else { alert(" you should work on your self more"); }
+
+
+
+ var job = prompt("do you have job experience ?(y/n) or (yes/no) ").toLocaleLowerCase();
+
+ while (job !== "yes" && job !== "no" && job !== "y" && job !== "n") {
+   job = prompt("please answer  yes / no / y / n").toLocaleLowerCase();
+
+ }
+
+ if (job === "yes" || job === "y") { alert(" Amaziiing , the experience nowadays is useful to have a job ");score++; }
+ else { alert(" So hooow you will get moneyyy!!!"); }
+
+
+
+ var purp = prompt("do you have purposes ?(y/n) or (yes/no) ").toLocaleLowerCase();
+
+ while (purp !== "yes" && purp !== "no" && purp !== "y" && purp !== "n") {
+   purp = prompt("please answer  yes / no / y / n").toLocaleLowerCase();
+
+ }
+
+ if (purp === "yes" || purp === "y") { alert(" You will be success,keep going to achieve them all ");score++; }
+ else { alert(" So why youuu still a live hhh "); }
+
+
+
+
+for (let j = 0; j <= 3; j++){
+
+var guessNum = prompt(" guess the number i choose in my mind between 1-20 ? please choose a numeric value");
+
+var num = parseInt(guessNum);
+//console.log(typeof(num));
+
+if ( num <= 10 && num >= 1) { alert("the guess is too low "); }
+else if ( num >= 11 && num <= 13 ) { alert("it is near value but still low"); }
+else if ( num >= 17 && num <= 20 ) { alert("it is near value but still high"); }
+else if ( num === 16 || num === 14) { alert("it is too near value "); }
+else if ( num == 15) { alert("its correct you are good guesser !");score++;
+break; } 
+//console.log(num);
 }
-  
 
-if ( locat === "yes" || locat === "y") { alert("WoW we are in the same country , nice to meet you "); }
-else { alert("Oh noo ! you must visit us!"); }
+alert(" the correct answer is 15");
 
 
 
-var userInput = prompt("are you feeling good?  (y/n) or (yes/no) ").toLocaleLowerCase();
 
 
-while (userInput !== "yes" && userInput !== "no" && userInput !== "y" && userInput !=="n") {
 
-  userInput = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
+
+function qSeven() {
+  var carType = ['bmw', 'kia', 'ford','porsche','lambo','ferrari','audi','honda','mercedes','bentley'];
+
+  for (var k = 0; k < 6; k++) {
+      var answerByUser = prompt('guess what is my best car type? Is it : bmw , kia , ford , porsche , lambo , ferrari , audi , honda , mercedes , bentley' );
+      var guess = answerByUser.toLowerCase();
+
+      
+          if (guess === carType[4]) {
+              alert('Correct Answer !!!');
+              score++;
+              
+              break;}
+          
+      
+      else { alert('wrong answer! please try again');}
+     
+         
+    
+     
+  }
 }
 
-if (userInput === "yes" || userInput ===  "y") { alert(" i wish more good feelings to u "); }
-else { alert(" try to do something you love , you will be better"); }
+qSeven();
+
+alert('My best car type is : lambo');
+
+alert(' You got ' + score + ' correct answers ! out of 7 questions ');
 
 
 
 
-
-var educ = prompt("do you have education history ?(y/n) or (yes/no)").toLocaleLowerCase();
-
-
-while (educ !== "yes" && educ !== "no" && educ !== "y" && educ !== "n") {
-  educ = prompt("please answer  yes / no / y / n ").toLocaleLowerCase();
-
-}
-
-if (educ === "yes" || educ === "y" ) { alert(" Niiice coz learning is a good thing "); }
-else { alert(" you should work on your self more"); }
-
-
-
-var job = prompt("do you have job experience ?(y/n) or (yes/no) ").toLocaleLowerCase();
-
-while (job !== "yes" && job !== "no" && job !== "y" && job !== "n") {
-  job = prompt("please answer  yes / no / y / n").toLocaleLowerCase();
-
-}
-
-if (job === "yes" || job === "y") { alert(" Amaziiing , the experience nowadays is useful to have a job "); }
-else { alert(" So hooow you will get moneyyy!!!"); }
-
-
-
-var purp = prompt("do you have purposes ?(y/n) or (yes/no) ").toLocaleLowerCase();
-
-while (purp !== "yes" && purp !== "no" && purp !== "y" && purp !== "n" ) {
-  purp = prompt("please answer  yes / no / y / n").toLocaleLowerCase();
-
-}
-
-if (purp === "yes" || purp === "y") { alert(" You will be success,keep going to achieve them all "); }
-else { alert(" So why youuu still a live hhh "); }
 
 
 
